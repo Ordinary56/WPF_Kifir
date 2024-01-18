f36140c6860fbdb9be0be3f501fbe683c3dbad17
Balogh Tamás <balogh.tamas@mechwart.local>
2024-01-18 14:07:54 +0100

    Added repo

---
+++ Repositories/KifirRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Kifir.Repositories
+{
+    internal class KifirRepository
+    {
+    }
+}
